11c2f6af60dc1fe0db276984c1593215c1c74b37
Michael Gentili <gentili@mcpnet.ca>
2018-03-16 17:28:45 +0000

    Add some booting sounds

---
+++ officelock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# vim /etc/tmux.conf
+#   new-session -s officelock /home/pi/git/officelock/officelock.sh
+#
+# sudo systemctl edit getty@tty1.service
+# sudo systemctl restart getty@tty1.service
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+figlet Booting
+while true; do
+    echo "."
+    aplay -q sounds/booting.wav
+done &
+PID=$!
+disown
+source /usr/local/bin/virtualenvwrapper.sh
+workon officelock
+kill $PID
+killall -q aplay
+figlet Initializing
+
+while true; do
+    ./officelock.py
+    aplay -q sounds/crash.wav
+done
