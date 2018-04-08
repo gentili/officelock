1fc8b87d80ee268e40ca62bb0027dfbfab466ff1
Michael Gentili <gentili@mcpnet.ca>
2018-04-08 01:56:11 +0000

    Add fun image picker for invalid codes

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Control system for bespoke raspberry pi based mag lock system
 ## /boot/config.txt
 sdtv_mode=0
 
+## Prereqs
+sudo apt install -y libjpeg8-dev libfreetype6-dev libwebp-dev
--- officelock.py
+++ officelock.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from pyfiglet import Figlet, figlet_format, print_figlet
+import os
 import getpass
 import curses
 from curses import (
@@ -15,6 +16,10 @@ from curses import (
 from subprocess import Popen, DEVNULL, run
 from time import sleep
 from datetime import datetime, timedelta
+from FBpyGIF import fb
+from itertools import cycle
+import imghdr
+from threading import Timer, Event
 
 fig_large = Figlet(
     font="cyberlarge",
@@ -25,6 +30,20 @@ fig_small = Figlet(
     justify="center",
 )
 
+def gif_loop(gif, duration):
+
+    imgs = fb.ready_gif(gif, True)
+    event = Event()
+    starttime = datetime.now()
+    for img, dur in cycle(imgs):
+        Timer(dur, lambda e:event.set(), [event]).start()
+        fb.show_img(img)
+        event.wait()
+        event.clear()
+        td = datetime.now() - starttime
+        if td > timedelta(seconds=duration):
+            return
+
 def playsound(soundname):
     Popen(["aplay", "sounds/{}.wav".format(soundname)],
           stdin=DEVNULL,
@@ -33,6 +52,7 @@ def playsound(soundname):
           )
 
 def main(stdscr):
+    curses.curs_set(False)
     curses.halfdelay(1)
     curses.init_pair(1, COLOR_GREEN, COLOR_BLACK)
     GREEN = curses.color_pair(1)
@@ -40,6 +60,7 @@ def main(stdscr):
     ERROR = curses.color_pair(2) | A_BOLD
     curses.init_pair(3, COLOR_WHITE, COLOR_GREEN)
     SUCCESS = curses.color_pair(3) | A_BOLD
+    fb.ready_fb()
     playsound('startup')
     while True:
         stdscr.bkgd(' ', 0)
@@ -129,6 +150,17 @@ def main(stdscr):
         elif passcode == "90210":
             return
 
+        elif passcode == "911":
+            playsound('accessgranted')
+            gif_loop(
+                fb.ready_img('../../Pictures/computer_art/fire__r10061297101.gif', False),
+                # fb.ready_img('../../Pictures/computer_art/lonely.gif', False),
+                10,
+            )
+            fb.black_scr()
+            continue
+
+
         elif passcode == "TOOLONG":
             playsound('error')
             errscreen("CODE TOO LONG")
@@ -142,6 +174,23 @@ def main(stdscr):
             errscreen("NO CODE ENTERED")
 
         else:
+            errscreen(passcode)
+            fullfilelist = []
+            for root, directories, filenames in os.walk('../../Pictures/'):
+                for filename in filenames:
+                    fullfilelist.append(os.path.join(root,filename))
+            try:
+                passnum = int(passcode)
+                if passnum < len(fullfilelist):
+                    playsound('warning')
+                    gif_loop(
+                        fb.ready_img(fullfilelist[passnum], False),
+                        10,
+                    )
+                    fb.black_scr()
+            except Exception:
+                pass
+
             playsound('invalidcode')
             errscreen("INVALID CODE")
         stdscr.refresh()
